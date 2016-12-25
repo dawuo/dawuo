a26459f1230f76498ac393513ac79652913296c5
E24034059 <hjpvul1765@gmail.com>
2016-12-25 22:54:06 +0800

    Delete db.js

--- models/db.js
+++
@@ -1,9 +0,0 @@
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://wp2016_groupC:abc@localhost/wp2016_groupC');
-var db =  mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback(){
-        console.log("Database Connected.");
-                                           });
-exports.mongoose = mongoose;
